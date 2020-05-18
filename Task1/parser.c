95b6221de6a6b95ce8a6e64e26715a763c2b5f2b
itay cohen <itay-co@yit.co.il>
2020-05-18 17:08:35 +0300

    more impl

--- Task1/parser.c
+++ Task1/parser.c
@@ -17,6 +17,12 @@ static void parse_FUNC_FULL_DEFS();
 static void parse_FUNC_PROTOTYPE();
 static void parse_RETURNED_TYPE();
 static void parse_PARAMS();
+static void parse_PARAM();
+static void parse_PARAM_TAG();
+static void parse_PARAM_TAG_TAG();
+static void parse_PARAM_LIST();
+static void parse_PARAM_LIST_TAG();
+static void parse_FUNC_PREDEFS_TAG();
 
 
 Token* nextToken;
@@ -173,25 +179,26 @@ static void parse_DIM_SIZES_TAG() {
 static void parse_GLOBAL_VARS_TAG() {
 	// GLOBAL_VARS_TAG supports epsilon so adding cases for Follow(GLOBAL_VARS_TAG):
 	// PROBLEM: some values for First(GLOBAL_VARS_TAG) and Follow(GLOBAL_VARS_TAG) are the same. (TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE)
-	// SOLVED BY: checks 2 steps ahead, if its TOKEN_SEMI_COLON - we will treat the mutual (TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE) as Follow(_GLOBAL_VARS_TAG)
-	// otherwise, if its TOKEN_ID we will treat the them as First(_GLOBAL_VARS_TAG). if no such of the two ahead exists this is an error.
+	// SOLVED BY: checks 3 steps ahead, if its TOKEN_SEMICOLON OR TOKEN_BRACKET_OPEN- we will treat the mutual (TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE) as First(_GLOBAL_VARS_TAG)
+	// otherwise, if its TOKEN_PARENTHESE_OPEN we will treat the them as Follow(_GLOBAL_VARS_TAG). if no such of the 3 ahead exists this is an error.
 	eTOKENS* self_follow;
-	eTOKENS twoAheadKind;
+	eTOKENS threeAheadKind;
 	nextToken = next_token();
-	twoAheadKind = next_token()->kind;
+	next_token();
+	threeAheadKind = next_token()->kind;
+	back_token();
 	back_token();
 	back_token();
 	switch (nextToken->kind) {
 		case TOKEN_INTEGER_TYPE:
 		case TOKEN_FLOAT_TYPE: {
-			if (twoAheadKind == TOKEN_ID) {
+			if (threeAheadKind == TOKEN_SEMICOLON || threeAheadKind == TOKEN_BRACKET_OPEN) {
 				//Treat as First(GLOBAL_VARS_TAG)
 				fprintf(yyout, "Rule GLOBAL_VARS_TAG-> VAR_DEC GLOBAL_VARS_TAG\n");
 				parse_VAR_DEC();
 				parse_GLOBAL_VARS_TAG();
 				break;
-			}
-			else if (twoAheadKind == TOKEN_SEMICOLON) {
+			} else if (threeAheadKind == TOKEN_PARENTHESE_OPEN) {
 				//Treat as Follow(GLOBAL_VARS_TAG)
 				fprintf(yyout, "Rule GLOBAL_VARS_TAG-> epsilon\n");
 				break;
@@ -222,6 +229,7 @@ static void parse_FUNC_PREDEFS() {
 			parse_FUNC_PROTOTYPE();
 			match(TOKEN_SEMICOLON);
 			parse_FUNC_PREDEFS_TAG();
+			break;
 		}
 		default: {
 			match_multi(3, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE, TOKEN_VOID_TYPE);
@@ -246,6 +254,7 @@ static void parse_FUNC_PROTOTYPE() {
 			match(TOKEN_PARENTHESE_OPEN);
 			parse_PARAMS();
 			match(TOKEN_PARENTHESE_CLOSE);
+			break;
 		}
 		default: {
 			match_multi(3, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE, TOKEN_VOID_TYPE);
@@ -255,9 +264,155 @@ static void parse_FUNC_PROTOTYPE() {
 	}
 }
 
+static void parse_RETURNED_TYPE() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_INTEGER_TYPE:
+		case TOKEN_FLOAT_TYPE: {
+			fprintf(yyout, "Rule RETURNED_TYPE -> TYPE\n");
+			parse_TYPE();
+			break;
+		}
+		case TOKEN_VOID_TYPE: {
+			fprintf(yyout, "Rule RETURNED_TYPE -> void\n");
+			match(TOKEN_VOID_TYPE);
+			break;
+		}
+		default: {
+			match_multi(3, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE, TOKEN_VOID_TYPE);
+			error_recovery(TOKEN_ID);
+		}
+	}
+}
+
+static void parse_PARAMS() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_INTEGER_TYPE:
+		case TOKEN_FLOAT_TYPE: {
+			fprintf(yyout, "Rule PARAMS -> PARAM_LIST\n");
+			parse_PARAM_LIST();
+			break;
+		}
+		// PARAMS supports epsilon so adding cases for Follow(PARAMS):
+		case TOKEN_PARENTHESE_CLOSE: {
+			fprintf(yyout, "Rule PARAMS -> epsilon\n");
+			break;
+		}
+		default: {
+			match_multi(2, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_PARAM_LIST() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_INTEGER_TYPE:
+		case TOKEN_FLOAT_TYPE: {
+			fprintf(yyout, "Rule PARAM_LIST -> PARAM PARAM_LIST_TAG\n");
+			parse_PARAM();
+			parse_PARAM_LIST_TAG();
+			break;
+		}	
+		default: {
+			match_multi(2, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_PARAM() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_INTEGER_TYPE:
+		case TOKEN_FLOAT_TYPE: {
+			fprintf(yyout, "Rule PARAM -> TYPE PARAM_TAG\n");
+			parse_TYPE();
+			parse_PARAM_TAG();
+			break;
+		}
+		default: {
+			match_multi(2, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_PARAM_TAG() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_ID: {
+			fprintf(yyout, "Rule PARAM_TAG -> id PARAM_TAG_TAG\n");
+			match(TOKEN_ID);
+			parse_PARAM_TAG_TAG();
+			break;
+		}
+		default: {
+			match(TOKEN_ID);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_PARAM_TAG_TAG() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_BRACKET_OPEN: {
+			fprintf(yyout, "Rule PARAM_TAG_TAG -> [ DIM_SIZES ]\n");
+			match(TOKEN_BRACKET_OPEN);
+			parse_DIM_SIZES();
+			match(TOKEN_BRACKET_CLOSE);
+			break;
+		}
+		// PARAM_TAG_TAG supports epsilon so adding cases for Follow(PARAM_TAG_TAG):
+		case TOKEN_PARENTHESE_CLOSE: {
+			fprintf(yyout, "Rule PARAM_TAG_TAG -> epsilon\n");
+			break;
+		}
+		default: {
+			match(TOKEN_ID);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_PARAM_LIST_TAG() {
+	nextToken = next_token();
+	back_token();
+	switch (nextToken->kind) {
+		case TOKEN_COMMA: {
+			fprintf(yyout, "Rule PARAM_LIST_TAG -> , PARAM PARAM_LIST_TAG\n");
+			match(TOKEN_COMMA);
+			parse_PARAM();
+			parse_PARAM_LIST_TAG();
+			break;
+		}
+		// PARAM_TAG_TAG supports epsilon so adding cases for Follow(PARAM_TAG_TAG):
+		case TOKEN_PARENTHESE_CLOSE: {
+			fprintf(yyout, "Rule PARAM_LIST_TAG -> epsilon\n");
+			break;
+		}
+		default: {
+			match_multi(2, TOKEN_INTEGER_TYPE, TOKEN_FLOAT_TYPE);
+			error_recovery(TOKEN_PARENTHESE_CLOSE);
+		}
+	}
+}
+
+static void parse_FUNC_PREDEFS_TAG() {
+	nextToken = next_token();
+}
+
 static void parse_FUNC_FULL_DEFS() {
 	nextToken = next_token();
-
 }
 
 /*
